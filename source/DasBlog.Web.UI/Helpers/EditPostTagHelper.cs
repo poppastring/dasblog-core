5187298806d15d778d5502a950c4dfe1790a81a9
Ryan Hill <ryanchill@hotmail.com>
2018-02-03 17:40:59 -0500

    #6 - post blog edit tag helper

---
+++ source/DasBlog.Web.UI/Helpers/EditPostTagHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using DasBlog.Web.UI.Models.BlogViewModels;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace DasBlog.Web.UI.Helpers
+{
+	public class EditPostTagHelper : TagHelper
+	{
+		public string Blog { get; set; }
+
+		public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+		{
+			output.TagName = "a";
+			output.TagMode = TagMode.StartTagAndEndTag;
+			output.Attributes.SetAttribute("href", $"Blogger/{Blog}/Edit"); //TODO: rest API subject to change
+			output.Content.SetHtmlContent("Edit this post");
+
+			return base.ProcessAsync(context, output);
+		}
+	}
+}
--- source/DasBlog.Web.UI/Helpers/PostCreatedTagHelper.cs
+++ source/DasBlog.Web.UI/Helpers/PostCreatedTagHelper.cs
@@ -2,20 +2,20 @@
 using DasBlog.Web.UI.Models.BlogViewModels;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 
-namespace DasBlog.Web.UI.Views
+namespace DasBlog.Web.UI.Helpers
 {
-    public class PostCreatedTagHelper : TagHelper
-    {
-        public PostViewModel Post { get; set; }
+	public class PostCreatedTagHelper : TagHelper
+	{
+		public PostViewModel Post { get; set; }
 
-        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
-        {
-            output.TagMode = TagMode.StartTagAndEndTag;
-            output.TagName = "p";
+		public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+		{
+			output.TagMode = TagMode.StartTagAndEndTag;
+			output.TagName = "p";
 
-            var content = await output.GetChildContentAsync();
-            string format = content.GetContent();
-            output.Content.SetHtmlContent(Post.CreatedDateTime.ToString(format));
-        }
-    }
+			var content = await output.GetChildContentAsync();
+			string format = content.GetContent();
+			output.Content.SetHtmlContent(Post.CreatedDateTime.ToString(format));
+		}
+	}
 }
