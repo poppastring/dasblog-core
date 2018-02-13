44f40dfef5d4468a9c796cc7728134d7b6783f87
Ryan Hill <ryanchill@hotmail.com>
2018-02-12 22:50:30 -0500

    #23 ASP.net core identity - fixing build break issues

--- source/DasBlog.Web.Repositories/Interfaces/ISiteSecurityRepository.cs
+++ source/DasBlog.Web.Repositories/Interfaces/ISiteSecurityRepository.cs
@@ -5,38 +5,38 @@ using System.Text;
 
 namespace DasBlog.Web.Repositories.Interfaces
 {
-    public interface ISiteSecurityRepository
-    {
-        bool IsValidContributor { get; }
+	public interface ISiteSecurityRepository
+	{
+		bool IsValidContributor { get; }
 
-        bool IsInRole(string role);
+		bool IsInRole(Role? role);
 
-        void AddUser(string userName, string password, string role, bool ask, string emailAddress);
+		void AddUser(string userName, string password, Role? role, bool ask, string emailAddress);
 
-        void AddUser(User user);
+		void AddUser(User user);
 
-        void UpdateUser(User user);
+		void UpdateUser(User user);
 
-        void Login(UserToken token, String userName);
+		void Login(UserToken token, String userName);
 
-        UserToken Login(string userName, string password);
+		UserToken Login(string userName, string password);
 
-        UserToken Login(string userName, string clientHash, string challenge);
+		UserToken Login(string userName, string clientHash, string challenge);
 
-        bool DoSuperChallenge(string challenge, string passwordIN, string userName, string clientHash);
+		bool DoSuperChallenge(string challenge, string passwordIN, string userName, string clientHash);
 
-        string Encrypt(string cleanString);
+		string Encrypt(string cleanString);
 
-        bool IsCleanStringEncrypted(string cleanString);
+		bool IsCleanStringEncrypted(string cleanString);
 
-        void SetPassword(string userName, string password);
+		void SetPassword(string userName, string password);
 
-        UserToken GetToken(string userName);
+		UserToken GetToken(string userName);
 
-        User GetUser(string userName);
+		User GetUser(string userName);
 
-        User GetUserByEmail(string email);
+		User GetUserByEmail(string email);
 
-        User GetUserByDisplayName(string displayName);
-    }
+		User GetUserByDisplayName(string displayName);
+	}
 }
--- source/DasBlog.Web.Repositories/SiteSecurityRepository.cs
+++ source/DasBlog.Web.Repositories/SiteSecurityRepository.cs
@@ -9,151 +9,151 @@ using System.Text;
 
 namespace DasBlog.Web.Repositories
 {
-    public class SiteSecurityRepository : ISiteSecurityRepository
-    {
-        private IPrincipal _principal;
-        private IDasBlogSettings _dasBlogSettings;
+	public class SiteSecurityRepository : ISiteSecurityRepository
+	{
+		private IPrincipal _principal;
+		private IDasBlogSettings _dasBlogSettings;
 
-        public SiteSecurityRepository(IPrincipal principal, IDasBlogSettings dasBlogSettings)
-        {
-            _dasBlogSettings = dasBlogSettings;
-            _principal = principal;
-        }
+		public SiteSecurityRepository(IPrincipal principal, IDasBlogSettings dasBlogSettings)
+		{
+			_dasBlogSettings = dasBlogSettings;
+			_principal = principal;
+		}
 
-        public bool IsValidContributor { get { return (_principal.IsInRole("") || _principal.IsInRole("")); } }
+		public bool IsValidContributor { get { return _principal.IsInRole(null); } }
 
-        public bool IsInRole(string role)
-        {
-            return _principal.IsInRole(role);
-        }
+		public bool IsInRole(Role? role)
+		{
+			return _principal.IsInRole(role.ToString());
+		}
 
-        public void AddUser(string userName, string password, string role, bool ask, string emailAddress)
-        {
-            if (string.IsNullOrEmpty(userName)) { throw new ArgumentNullException("userName"); }
-            if (string.IsNullOrEmpty(password)) { throw new ArgumentNullException("password"); }
+		public void AddUser(string userName, string password, Role? role, bool ask, string emailAddress)
+		{
+			if (string.IsNullOrEmpty(userName)) { throw new ArgumentNullException(nameof(userName)); }
+			if (string.IsNullOrEmpty(password)) { throw new ArgumentNullException(nameof(password)); }
 
-            User user = new User
-            {
-                Name = userName,
-                Password = this.Encrypt(password),
-                Role = role ?? "contributor",
-                Ask = ask,
-                EmailAddress = emailAddress
-            };
+			User user = new User
+			{
+				Name = userName,
+				Password = this.Encrypt(password),
+				Role = role ?? Role.Contrinutor,
+				Ask = ask,
+				EmailAddress = emailAddress
+			};
 
-            AddUser(user);
-        }
+			AddUser(user);
+		}
 
-        public void AddUser(User user)
-        {
-            if (user == null)
-            {
-                throw new ArgumentNullException("user");
-            }
+		public void AddUser(User user)
+		{
+			if (user == null)
+			{
+				throw new ArgumentNullException("user");
+			}
 
-            _dasBlogSettings.SecurityConfiguration.Users.Add(user);
-        }
+			_dasBlogSettings.SecurityConfiguration.Users.Add(user);
+		}
 
-        public User GetUser(string userName)
-        {
-            return _dasBlogSettings.SecurityConfiguration.Users.FirstOrDefault(s => string.Compare(s.Name, userName, StringComparison.InvariantCultureIgnoreCase) == 0);
-        }
+		public User GetUser(string userName)
+		{
+			return _dasBlogSettings.SecurityConfiguration.Users.FirstOrDefault(s => string.Compare(s.Name, userName, StringComparison.InvariantCultureIgnoreCase) == 0);
+		}
 
-        public User GetUserByDisplayName(string displayName)
-        {
-            return _dasBlogSettings.SecurityConfiguration.Users.FirstOrDefault(s => string.Compare(s.DisplayName, displayName, StringComparison.InvariantCultureIgnoreCase) == 0);
-        }
+		public User GetUserByDisplayName(string displayName)
+		{
+			return _dasBlogSettings.SecurityConfiguration.Users.FirstOrDefault(s => string.Compare(s.DisplayName, displayName, StringComparison.InvariantCultureIgnoreCase) == 0);
+		}
 
-        public User GetUserByEmail(string email)
-        {
-            return _dasBlogSettings.SecurityConfiguration.Users.FirstOrDefault(s => string.Compare(s.EmailAddress, email, StringComparison.InvariantCultureIgnoreCase) == 0);
-        }
+		public User GetUserByEmail(string email)
+		{
+			return _dasBlogSettings.SecurityConfiguration.Users.FirstOrDefault(s => string.Compare(s.EmailAddress, email, StringComparison.InvariantCultureIgnoreCase) == 0);
+		}
 
-        public void UpdateUser(User user)
-        {
-            throw new NotImplementedException();
-        }
+		public void UpdateUser(User user)
+		{
+			throw new NotImplementedException();
+		}
 
-        public bool DoSuperChallenge(string challenge, string passwordIN, string userName, string clientHash)
-        {
-            throw new NotImplementedException();
-        }
+		public bool DoSuperChallenge(string challenge, string passwordIN, string userName, string clientHash)
+		{
+			throw new NotImplementedException();
+		}
 
-        public string Encrypt(string cleanString)
-        {
-            if (!IsCleanStringEncrypted(cleanString))
-            {
-                Byte[] clearBytes = Encoding.Unicode.GetBytes(cleanString);
+		public string Encrypt(string cleanString)
+		{
+			if (!IsCleanStringEncrypted(cleanString))
+			{
+				Byte[] clearBytes = Encoding.Unicode.GetBytes(cleanString);
 
-                MD5 md5 = MD5CryptoServiceProvider.Create();
-                Byte[] hashedBytes = md5.ComputeHash(clearBytes);
+				MD5 md5 = MD5CryptoServiceProvider.Create();
+				Byte[] hashedBytes = md5.ComputeHash(clearBytes);
 
-                return BitConverter.ToString(hashedBytes);
-            }
-            else
-                return cleanString;
-        }
+				return BitConverter.ToString(hashedBytes);
+			}
+			else
+				return cleanString;
+		}
 
-        public UserToken GetToken(string userName)
-        {
-            User user = GetUser(userName);
-            return user?.ToToken();
-        }
+		public UserToken GetToken(string userName)
+		{
+			User user = GetUser(userName);
+			return user?.ToToken();
+		}
 
-        public bool IsCleanStringEncrypted(string cleanString)
-        {
-            return (cleanString.Length == 47 && cleanString.Replace("-", String.Empty).Length == 32);
-        }
+		public bool IsCleanStringEncrypted(string cleanString)
+		{
+			return (cleanString.Length == 47 && cleanString.Replace("-", String.Empty).Length == 32);
+		}
 
-        public void Login(UserToken token, string userName)
-        {
-            if (token == null)
-            {
-                // SiteSecurity.LogFailure(userName);
-            }
-            else
-            {
-                // SiteSecurity.LogSuccess(token.Name);
-                GenericIdentity identity = new GenericIdentity(token.Name, "Custom");
-                GenericPrincipal principal = new GenericPrincipal(identity, new string[] { token.Role });
-                // HttpContext.Current.User = principal;
-                System.Threading.Thread.CurrentPrincipal = principal;
-            }
-        }
+		public void Login(UserToken token, string userName)
+		{
+			if (token == null)
+			{
+				// SiteSecurity.LogFailure(userName);
+			}
+			else
+			{
+				// SiteSecurity.LogSuccess(token.Name);
+				GenericIdentity identity = new GenericIdentity(token.Name, "Custom");
+				GenericPrincipal principal = new GenericPrincipal(identity, new string[] { token.Role });
+				// HttpContext.Current.User = principal;
+				System.Threading.Thread.CurrentPrincipal = principal;
+			}
+		}
 
-        public UserToken Login(string userName, string password)
-        {
-            UserToken token = null;
+		public UserToken Login(string userName, string password)
+		{
+			UserToken token = null;
 
-            User user = GetUser(userName);
+			User user = GetUser(userName);
 
-            if (user != null && user.Active)
-            {
-                //Make sure password is encrypted
-                if (!IsCleanStringEncrypted(password))
-                {
-                    password = Encrypt(password);
-                }
+			if (user != null && user.Active)
+			{
+				//Make sure password is encrypted
+				if (!IsCleanStringEncrypted(password))
+				{
+					password = Encrypt(password);
+				}
 
-                if ((IsCleanStringEncrypted(user.Password) && user.Password == password) || (Encrypt(user.Password) == password))
-                {
-                    token = user.ToToken();
-                }
-            }
+				if ((IsCleanStringEncrypted(user.Password) && user.Password == password) || (Encrypt(user.Password) == password))
+				{
+					token = user.ToToken();
+				}
+			}
 
-            Login(token, userName);
+			Login(token, userName);
 
-            return token;
-        }
+			return token;
+		}
 
-        public UserToken Login(string userName, string clientHash, string challenge)
-        {
-            throw new NotImplementedException();
-        }
+		public UserToken Login(string userName, string clientHash, string challenge)
+		{
+			throw new NotImplementedException();
+		}
 
-        public void SetPassword(string userName, string password)
-        {
-            throw new NotImplementedException();
-        }
-    }
+		public void SetPassword(string userName, string password)
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
