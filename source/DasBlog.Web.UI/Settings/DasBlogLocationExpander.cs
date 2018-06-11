6d1804e3d601c18a2e100a489cd663b061437a58
Mark Downie <poppastring@users.noreply.github.com>
2018-06-11 19:12:39 -0400

    Changed location of shared partial views

--- source/DasBlog.Web.UI/Settings/DasBlogLocationExpander.cs
+++ source/DasBlog.Web.UI/Settings/DasBlogLocationExpander.cs
@@ -6,18 +6,23 @@ namespace DasBlog.Web.Settings
 {
     public class DasBlogLocationExpander : IViewLocationExpander
     {
-        private const string _themeLocation = "/Themes/{0}";
-        private string _theme;
+        private const string _themeLocation = "/Themes/{0}/{1}";
+		private const string _defaultViewLocation = "/Views/Shared/{0}.cshtml";
+		private string _theme;
 
         public DasBlogLocationExpander(string theme)
         {
-            _theme = string.Format(_themeLocation, theme);
-        }
+            _theme = string.Format(_themeLocation, theme, "{0}.cshtml");
+		}
 
         public IEnumerable<string> ExpandViewLocations(ViewLocationExpanderContext context, IEnumerable<string> viewLocations)
         {
-            return viewLocations.Select(s => s.Replace("/Views/Shared", _theme));
-        }
+			var listlocations = viewLocations.ToList();
+			listlocations.Add(_theme);
+			return listlocations;
+
+			// return viewLocations.Select(s => s.Replace(_defaultViewLocation, _theme));
+		}
 
         public void PopulateValues(ViewLocationExpanderContext context)
         {
