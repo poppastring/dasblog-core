5b79e6d23ef21a0e32f1157b1aeaa71e9726914f
Mark Downie <poppastring@users.noreply.github.com>
2018-06-14 23:20:20 -0400

    Template of the Authorization Role

---
+++ source/DasBlog.Web.UI/TagHelpers/AuthorizationRoleTagHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace DasBlog.Web.TagHelpers
+{
+	public class AuthorizationRoleTagHelper : TagHelper
+	{
+		public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+		{
+			var content = await output.GetChildContentAsync();
+		}
+	}
+}
