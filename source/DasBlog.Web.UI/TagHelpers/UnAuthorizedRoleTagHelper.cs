a8c34691a89ac512e093124ca6f38b82b5be0b0d
Mark Downie <poppastring@users.noreply.github.com>
2018-06-15 22:59:06 -0400

    Created an Authorized and UnAuthorized tag helper to help hide admin related links

--- source/DasBlog.Web.UI/TagHelpers/UnAuthorizedRoleTagHelper.cs
+++ source/DasBlog.Web.UI/TagHelpers/UnAuthorizedRoleTagHelper.cs
@@ -9,38 +9,36 @@ using Microsoft.AspNetCore.Razor.TagHelpers;
 
 namespace DasBlog.Web.TagHelpers
 {
-	[HtmlTargetElement(Attributes = "dasblog-authorized")]
-	[HtmlTargetElement(Attributes = "dasblog-authorized,dasblog-roles")]
-	public class AuthorizationRoleTagHelper : TagHelper, IAuthorizeData
+	[HtmlTargetElement(Attributes = "dasblog-unauthorized")]
+	public class UnAuthorizedRoleTagHelper : TagHelper, IAuthorizeData
 	{
 		private readonly IPolicyEvaluator _policyEvaluator;
 		private readonly IAuthorizationPolicyProvider _authPolicyProvider;
 		private readonly IHttpContextAccessor _httpContextAccessor;
 
-		[HtmlAttributeName("dasblog-roles")]
-		public string Roles { get; set; }
-
-		public string Policy { get; set; }
-
-		public string AuthenticationSchemes { get; set; }
-
-		public AuthorizationRoleTagHelper(IHttpContextAccessor httpContextAccessor, IAuthorizationPolicyProvider policyProvider, IPolicyEvaluator policyEvaluator)
+		public UnAuthorizedRoleTagHelper(IHttpContextAccessor httpContextAccessor, IAuthorizationPolicyProvider policyProvider, IPolicyEvaluator policyEvaluator)
 		{
 			_httpContextAccessor = httpContextAccessor;
 			_authPolicyProvider = policyProvider;
 			_policyEvaluator = policyEvaluator;
 		}
 
+		public string Policy { get; set; }
+
+		public string Roles { get; set; }
+
+		public string AuthenticationSchemes { get; set; }
+
 		public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
 		{
 			var policy = await AuthorizationPolicy.CombineAsync(_authPolicyProvider, new[] { this });
 			var authenticateResult = await _policyEvaluator.AuthenticateAsync(policy, _httpContextAccessor.HttpContext);
 			var authorizeResult = await _policyEvaluator.AuthorizeAsync(policy, authenticateResult, _httpContextAccessor.HttpContext, null);
-			
-			if (!authorizeResult.Succeeded)
+
+			if (authorizeResult.Succeeded)
 			{
 				output.SuppressOutput();
-			}	
+			}
 		}
 	}
 }
