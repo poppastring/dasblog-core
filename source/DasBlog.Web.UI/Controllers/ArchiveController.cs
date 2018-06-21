e8cceb5644a1ea3914404dc1ee00d2a80cbdbff7
Ryan Hill <ryanchill@hotmail.com>
2018-06-21 11:46:08 -0400

    #36 UI for archive view

--- source/DasBlog.Web.UI/Controllers/ArchiveController.cs
+++ source/DasBlog.Web.UI/Controllers/ArchiveController.cs
@@ -1,61 +1,66 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using newtelligence.DasBlog.Runtime;
-using DasBlog.Web;
+using AutoMapper;
 using DasBlog.Managers.Interfaces;
+using DasBlog.Web.Models.BlogViewModels;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DasBlog.Web.Controllers
 {
-    [Route("archive")]
-    public class ArchiveController : Controller
-    {
-        private IArchiveManager _archiveManager;
-        private IHttpContextAccessor _httpContextAccessor;
+	[Route("archive")]
+	public class ArchiveController : Controller
+	{
+		private IArchiveManager _archiveManager;
+		private IHttpContextAccessor _httpContextAccessor;
+		private readonly IMapper mapper;
 
-        public ArchiveController(IArchiveManager archiveManager, IHttpContextAccessor httpContextAccessor)
-        {
-            _archiveManager = archiveManager;
-            _httpContextAccessor = httpContextAccessor;
-        }
+		public ArchiveController(IArchiveManager archiveManager, IHttpContextAccessor httpContextAccessor, IMapper mapper)
+		{
+			_archiveManager = archiveManager;
+			_httpContextAccessor = httpContextAccessor;
+			this.mapper = mapper;
+		}
 
-        [HttpGet("")]
-        public IActionResult Index()
-        {
-            return Archive(DateTime.Now.Year);
-        }
+		[HttpGet("")]
+		public IActionResult Archive()
+		{
+			return Archive(DateTime.Now.Year, DateTime.Now.Month);
+		}
 
-        [HttpGet("{year}")]
-        public IActionResult Archive(int year)
-        {
-            DateTime dateTime = new DateTime(year, 1, 1);
-            string languageFilter = _httpContextAccessor.HttpContext.Request.Headers["Accept-Language"];
+		[HttpGet("{year}")]
+		public IActionResult Archive(int year)
+		{
+			DateTime dateTime = new DateTime(year, 1, 1);
+			var months = GetMonthsViewModel(dateTime);
+			return View(months);
+		}
 
-            var months = _archiveManager.GetEntriesForYear(dateTime, languageFilter);
-            return View("", months);
-        }
+		[HttpGet("{year}/{month}")]
+		public IActionResult Archive(int year, int month)
+		{
+			DateTime dateTime = new DateTime(year, month, 1);
+			var months = GetMonthsViewModel(dateTime);
+			return View(months);
+		}
 
-        [HttpGet("{year}/{month}")]
-        public IActionResult Archive(int year, int month)
-        {
-            DateTime dateTime = new DateTime(year, month, 1);
-            string languageFilter = _httpContextAccessor.HttpContext.Request.Headers["Accept-Language"];
+		[HttpGet("{year}/{month}/{day}")]
+		public IActionResult Archive(int year, int month, int day)
+		{
+			DateTime dateTime = new DateTime(year, month, day);
+			var months = GetMonthsViewModel(dateTime);
+			return View(months);
+		}
 
-            var months = _archiveManager.GetEntriesForMonth(dateTime, languageFilter);
-            return View("", months);
-        }
+		private MonthViewViewModel GetMonthsViewModel(DateTime dateTime)
+		{
+			string languageFilter = _httpContextAccessor.HttpContext.Request.Headers["Accept-Language"];
 
-        [HttpGet("{year}/{month}/{day}")]
-        public IActionResult Archive(int year, int month, int day)
-        {
-            DateTime dateTime = new DateTime(year, month, day);
-            string languageFilter = _httpContextAccessor.HttpContext.Request.Headers["Accept-Language"];
-
-            var months = _archiveManager.GetEntriesForMonth(dateTime, languageFilter);
-            return View("", months);
-        }
-    }
+			ViewBag.PreviousMonth = dateTime.AddMonths(-1).Date;
+			ViewBag.NextMonth = dateTime.AddMonths(1).Date;
+			ViewBag.CurrentMonth = dateTime.Date;
+			var entries = _archiveManager
+				.GetEntriesForMonth(dateTime, languageFilter);
+			return MonthViewViewModel.Create(dateTime, entries, mapper);
+		}
+	}
 }
