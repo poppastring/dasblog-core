068edf212a49f3ba91a2a48d6e724f04c28a46d1
Mark Downie <poppastring@users.noreply.github.com>
2018-06-21 22:35:39 -0400

    Update a couple of changes to category code and layout. Looks more like the original dasblog category page

--- source/DasBlog.Web.UI/Models/BlogViewModels/CategoryPostItem.cs
+++ source/DasBlog.Web.UI/Models/BlogViewModels/CategoryPostItem.cs
@@ -2,13 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using newtelligence.DasBlog.Runtime;
 
 namespace DasBlog.Web.Models.BlogViewModels
 {
     public class CategoryPostItem
     {
+		public string Category { get; set; }
+
 		public string BlogTitle { get; set; }
 
 		public string BlogId { get; set; }
+
+		public static CategoryPostItem CreateFromEntry(Entry entry)
+		{
+			return new CategoryPostItem
+			{
+				Category = entry.GetSplitCategories().FirstOrDefault(),
+				BlogTitle = entry.Title,
+				BlogId = entry.CompressedTitle
+			};
+		}
 	}
 }
