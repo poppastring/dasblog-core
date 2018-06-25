8113188adaa5f76bf9125282a25694a582cda3e6
mike <mikemay@blueyonder.co.uk>
2018-06-25 18:56:07 +0100

    delete-post substantially complete

---
+++ source/DasBlog.Web.UI/TagHelpers/DeletePostTagHelper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Threading.Tasks;
+
+namespace DasBlog.Web.TagHelpers
+{
+	public class DeletePostTagHelper : TagHelper
+	{
+		public string BlogPostId { get; set; }
+
+		public override void Process(TagHelperContext context, TagHelperOutput output)
+		{
+			output.TagName = "a";
+			output.TagMode = TagMode.StartTagAndEndTag;
+			output.Attributes.SetAttribute("id", "deletePost");
+			output.Attributes.SetAttribute("href", $"post/{BlogPostId}/delete");
+			output.Content.SetHtmlContent("Delete this post");
+		}
+
+		public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+		{
+			return Task.Run(() => Process(context, output));
+		}
+	}
+}
