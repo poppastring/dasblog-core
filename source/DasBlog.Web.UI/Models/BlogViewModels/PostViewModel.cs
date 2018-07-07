e9fa79fa5f9934d060ab61da9d0ef78957810cd9
mike <mikemay@blueyonder.co.uk>
2018-07-07 06:23:21 +0100

    fixed broken build

--- source/DasBlog.Web.UI/Models/BlogViewModels/PostViewModel.cs
+++ source/DasBlog.Web.UI/Models/BlogViewModels/PostViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace DasBlog.Web.Models.BlogViewModels
