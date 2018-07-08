1f3f95184d8d3c97c862defe4825de35b3b76fa3
mike <mikemay@blueyonder.co.uk>
2018-07-08 13:53:02 +0100

    started on user views

---
+++ source/DasBlog.Web.UI/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DasBlog.Core.Security;
+using Microsoft.AspNetCore.Mvc;
+using DasBlog.Core.Services.Interfaces;
+using DasBlog.Web.Models;
+
+namespace DasBlog.Web.Controllers
+{
+	public class UsersController : Controller
+	{
+		private readonly ILocalUserDataService _userService;
+		public UsersController(ILocalUserDataService userService)
+		{
+			_userService = userService;
+		}
+		[Route("users")]
+		public IActionResult Index()
+		{
+			ViewBag.SubViewName = "ViewUser";
+			List<User> users = _userService.LoadUsers().ToList();
+			var uvm = new UsersViewModel(users[0]);
+			return View("Maintenance", uvm);
+		}
+	}
+}
---
+++ source/DasBlog.Web.UI/Models/UsersViewModel.cs
@@ -0,0 +1,15 @@
+﻿using DasBlog.Core.Security;
+
+namespace DasBlog.Web.Models
+{
+	public class UsersViewModel
+	{
+		private readonly User _user;
+		public UsersViewModel(User user)
+		{
+			_user = user;
+		}
+
+		public User User => _user;
+	}
+}
