1596ece4a9595a33a76b8f75014d686b27b7cd34
mike <mikemay@blueyonder.co.uk>
2018-07-08 18:16:13 +0100

    user views WIP

---
+++ source/DasBlog.Web.UI/Views/Components/UserList.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using DasBlog.Core.Security;
+using Microsoft.AspNetCore.Mvc;
+using DasBlog.Core.Services.Interfaces;
+
+namespace DasBlog.Web.Components
+{
+	public class UserList : ViewComponent
+	{
+		private ILocalUserDataService _dataService;
+		public UserList( ILocalUserDataService dataService)
+		{
+			this._dataService = dataService;
+           
+		}
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			IEnumerable<User> list = _dataService.LoadUsers();
+			await Task.Yield();
+			return View(list);
+		}
+        
+	}
+}
