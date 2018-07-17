6838c60eaca0c9ae4401fc617ca2ced48872be2b
mike <mikemay@blueyonder.co.uk>
2018-07-17 08:21:40 +0100

    code plugin added to tinymce + reasonable sizing of tinymce

--- source/DasBlog.Web.UI/TagHelpers/RichEdit/TinyMceBuilder.cs
+++ source/DasBlog.Web.UI/TagHelpers/RichEdit/TinyMceBuilder.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+﻿using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
 
 namespace DasBlog.Web.TagHelpers.RichEdit
 {
@@ -6,9 +7,11 @@ namespace DasBlog.Web.TagHelpers.RichEdit
 	{
 		private const string TINY_MCE_SERVICE_URL = "https://cloud.tinymce.com/stable/tinymce.min.js";
 		private const string INIT_SCRIPT_TEMPLATE = @"
+		<script language=""javascript"" type=""text/javascript"" src=""/js/tinymce/plugins/code/plugin.min.js""></script>
 		<script>
 		tinymce.init({{
 			selector: '#{0}'
+			,plugins: 'code'
 		}});
 		</script>
 		";
@@ -20,7 +23,11 @@ namespace DasBlog.Web.TagHelpers.RichEdit
 			output.Attributes.SetAttribute("comment", "a rich-edit-scripts element should be included on the page");
 			output.Attributes.SetAttribute("id", tagHelper.Id);
 			output.Attributes.SetAttribute("name", tagHelper.Name);
-			output.Attributes.SetAttribute("style", "width: 100%; height: 90%;");
+			// the following is blatant cargo-culting to tet  a resonable size
+			// just copied the legacy settings - no idea why it works
+			output.Attributes.SetAttribute("style", "height=400px; width=99%;");
+			output.Attributes.SetAttribute("cols", "20");
+			output.Attributes.SetAttribute("rows","2");
 		}
 
 		public void ProcessScripts(RichEditScriptsTagHelper tagHeelper, TagHelperContext context, TagHelperOutput output)
@@ -28,6 +35,8 @@ namespace DasBlog.Web.TagHelpers.RichEdit
 			output.TagName = "script";
 			output.TagMode = TagMode.StartTagAndEndTag;
 			output.Attributes.SetAttribute("src", TINY_MCE_SERVICE_URL);
+			output.Attributes.SetAttribute("type", "text/javascript");
+			output.Attributes.SetAttribute("language", "javascript");
 			string htmlContent = string.Format(INIT_SCRIPT_TEMPLATE, tagHeelper.ControlId);
 			output.PostElement.SetHtmlContent(htmlContent);
 		}
