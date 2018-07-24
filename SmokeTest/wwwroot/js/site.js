99fa709a7b3fb62a9cb8392ff680dc4702674c8e
mike <mikemay@blueyonder.co.uk>
2018-07-24 18:36:58 +0100

    added smoke test static files

---
+++ SmokeTest/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿// Write your JavaScript code.
+function deleteEntry(entryId, entryTitle) {
+    if (confirm("Are you sure you want to delete this item? \n\n" + entryTitle)) {
+        location.href = "post/" + entryId + "/delete"
+    }
+}    
+function linkToUser(emailAddress, linkAbility) {
+    if (linkAbility === "disabled") {
+        alert("You must complete the current operation (create, edit or delete user), before you can select another");
+        return;
+    }
+    location.href = emailAddress;
+}
