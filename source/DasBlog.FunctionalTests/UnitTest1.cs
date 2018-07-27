d589b2cb83262a23bc3ba5f2d49aebc8466b1392
mike <mikemay@blueyonder.co.uk>
2018-07-27 05:03:45 +0100

    testing AppVeyor with Selenium

---
+++ source/DasBlog.FunctionalTests/UnitTest1.cs
@@ -0,0 +1,43 @@
+using System;
+using System.IO;
+using System.Reflection;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using Xunit;
+
+namespace DasBlog.FunctionalTests
+{
+	public class AutomateThePlanetTestsXUnit
+	{
+		[Fact]
+		public void TestWithFirefoxDriver()
+		{
+			using (var driver = new FirefoxDriver())
+			{
+				driver.Navigate().GoToUrl(@"https://automatetheplanet.com/multiple-files-page-objects-item-templates/");
+				var link = driver.FindElement(By.PartialLinkText("TFS Test API"));
+				var jsToBeExecuted = $"window.scroll(0, {link.Location.Y});";
+				((IJavaScriptExecutor)driver).ExecuteScript(jsToBeExecuted);
+				var wait = new WebDriverWait(driver, TimeSpan.FromMinutes(1));
+				var clickableElement = wait.Until(ExpectedConditions.ElementToBeClickable(By.PartialLinkText("TFS Test API")));
+				clickableElement.Click();
+			}
+		}
+		[Fact]
+		public void TestWithChromeDriver()
+		{
+			using (var driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)))
+			{
+				driver.Navigate().GoToUrl(@"https://automatetheplanet.com/multiple-files-page-objects-item-templates/");
+				var link = driver.FindElement(By.PartialLinkText("TFS Test API"));
+				var jsToBeExecuted = $"window.scroll(0, {link.Location.Y});";
+				((IJavaScriptExecutor)driver).ExecuteScript(jsToBeExecuted);
+				var wait = new WebDriverWait(driver, TimeSpan.FromMinutes(1));
+				var clickableElement = wait.Until(ExpectedConditions.ElementToBeClickable(By.PartialLinkText("TFS Test API")));
+				clickableElement.Click();
+			}
+		}
+	}
+}
