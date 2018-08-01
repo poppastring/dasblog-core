8c92a9126b22a2bfefa625b304a2ca09545601b2
Mark Downie <poppastring@users.noreply.github.com>
2018-07-31 23:21:46 -0400

    Fixing the social tags...

---
+++ source/DasBlog.Web.UI/TagHelpers/PostToFacebookTagHelper.cs
@@ -0,0 +1,32 @@
+﻿using DasBlog.Core;
+using DasBlog.Web.Models.BlogViewModels;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Threading.Tasks;
+
+namespace DasBlog.Web.TagHelpers
+{
+	public class PostToFacebookTagHelper : TagHelper
+	{
+		private const string FACEBOOK_SHARE_URL = "https://facebook.com/sharer.php?u={0}";
+		private IDasBlogSettings dasBlogSettings;
+		public PostViewModel Post { get; set; }
+
+		public PostToFacebookTagHelper(IDasBlogSettings dasBlogSettings)
+		{
+			this.dasBlogSettings = dasBlogSettings;
+		}
+
+		public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+		{
+			string categorylist = string.Empty;
+			output.TagName = "a";
+			output.TagMode = TagMode.StartTagAndEndTag;
+			output.Attributes.SetAttribute("class", "dasblog-a-share-facebook");
+			output.Attributes.SetAttribute("href", string.Format(FACEBOOK_SHARE_URL, dasBlogSettings.GetBaseUrl() + Post.PermaLink));
+
+			var content = await output.GetChildContentAsync();
+
+			output.Content.SetHtmlContent(content.GetContent());
+		}
+	}
+}
