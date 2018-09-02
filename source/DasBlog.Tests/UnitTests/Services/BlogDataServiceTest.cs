7a7e988f301aeee1fa8510baf405846191b459b0
Mark Downie <poppastring@users.noreply.github.com>
2018-09-01 22:38:51 -0400

    Included test data Unit Test constants (may still need more work) Check for entries.

---
+++ source/DasBlog.Tests/UnitTests/Services/BlogDataServiceTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using newtelligence.DasBlog.Runtime;
+using NodaTime;
+using Xunit;
+
+namespace DasBlog.Tests.UnitTests.Services
+{
+    public class BlogDataServiceTest
+    {
+		/// <summary>
+		/// All test methods should follow this naming pattern
+		/// </summary>
+		[Fact]
+		public void UnitOfWork_StateUnderTest_ExpectedBehavior()
+		{
+
+		}
+
+		[Theory]
+		[MemberData(nameof(DasBlogDataService))]
+		public void BlogDataService_EntryRetrieval_Successful(IBlogDataService blogdataservice)
+		{
+			var entries = blogdataservice.GetEntriesForDay(DateTime.MaxValue.AddDays(-2), DateTimeZone.Utc, string.Empty, int.MaxValue, int.MaxValue, string.Empty);
+			Assert.NotNull(entries);
+		}
+
+		public static TheoryData<IBlogDataService> DasBlogDataService = new TheoryData<IBlogDataService>
+		{
+			BlogDataServiceFactory.GetService(UnitTestsConstants.TestContentLocation, LoggingDataServiceFactory.GetService(UnitTestsConstants.TestLoggingLocation))
+		};
+
+
+	}
+}
---
+++ source/DasBlog.Tests/UnitTests/UnitTestsConstants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using newtelligence.DasBlog.Util;
+
+namespace DasBlog.Tests.UnitTests
+{
+	public class UnitTestsConstants
+	{
+		private static readonly DirectoryInfo root = new DirectoryInfo(ReflectionHelper.CodeBase());
+
+		public static string TestContentLocation { get { return new DirectoryInfo(Path.Combine(root.Parent.FullName, "netcoreapp2.1\\TestContent")).FullName; } }
+
+		public static string TestLoggingLocation { get { return new DirectoryInfo(Path.Combine(root.Parent.FullName, "netcoreapp2.1\\logs")).FullName; } }
+	}
+}
