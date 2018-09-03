50cdf8b06ddd57d7e3472d1076c1b87bdb2ea8f8
Mark Downie <poppastring@users.noreply.github.com>
2018-09-02 22:31:06 -0400

    More tests for BlogDataServices. Should be a good enough start.

--- source/DasBlog.Tests/UnitTests/Services/BlogDataServiceTest.cs
+++ source/DasBlog.Tests/UnitTests/Services/BlogDataServiceTest.cs
@@ -9,18 +9,78 @@ namespace DasBlog.Tests.UnitTests.Services
 {
     public class BlogDataServiceTest
     {
-		/// <summary>
-		/// All test methods should follow this naming pattern
-		/// </summary>
-		[Fact]
-		public void UnitOfWork_StateUnderTest_ExpectedBehavior()
+		[Theory]
+		[MemberData(nameof(DasBlogDataService))]
+		[Trait("Category", "UnitTest")]
+		public void BlogDataService_CreateGetDeleteEntry_Successful(IBlogDataService blogdataservice)
 		{
+			var entry = TestEntry.CreateEntry(String.Format("Test Entry"), 5, 2);
+			blogdataservice.SaveEntry(entry, null);
 
+			var dt = DateTime.Now;
+			var returnEntry = blogdataservice.GetEntry(entry.EntryId);
+			Assert.Equal(0, entry.CompareTo(returnEntry));
+
+			blogdataservice.DeleteEntry(entry.EntryId, null);
+
+			var entryFailRetrieval = blogdataservice.GetEntry(entry.EntryId);
+			Assert.Null(entryFailRetrieval);
 		}
 
 		[Theory]
 		[MemberData(nameof(DasBlogDataService))]
-		public void BlogDataService_EntryRetrieval_Successful(IBlogDataService blogdataservice)
+		[Trait("Category", "UnitTest")]
+		public void BlogDataService_GetEntryForEditDelete_Successful(IBlogDataService blogdataservice)
+		{
+			var entry = TestEntry.CreateEntry(String.Format("Test Entry"), 5, 2);
+			blogdataservice.SaveEntry(entry, null);
+
+			var dt = DateTime.Now;
+			var returnEntry = blogdataservice.GetEntryForEdit(entry.EntryId);
+			Assert.NotNull(returnEntry);
+
+			blogdataservice.DeleteEntry(entry.EntryId, null);
+
+			var entryFailRetrieval = blogdataservice.GetEntry(entry.EntryId);
+			Assert.Null(entryFailRetrieval);
+		}
+
+		[Theory]
+		[MemberData(nameof(DasBlogDataService))]
+		[Trait("Category", "UnitTest")]
+		public void BlogDataService_UpdateEntriesDelete_Successful(IBlogDataService blogdataservice)
+		{
+			var entry = TestEntry.CreateEntry(String.Format("Test Entry"), 5, 2);
+			entry.Categories = "test";
+			blogdataservice.SaveEntry(entry, null);
+
+			var dt = DateTime.Now;
+			var returnEntry = blogdataservice.GetEntryForEdit(entry.EntryId);
+			Assert.NotNull(returnEntry);
+
+			returnEntry.Title = "Test Entry 2";
+			var state = blogdataservice.SaveEntry(returnEntry, null);
+			Assert.True(state == EntrySaveState.Updated);
+
+			var entryAfterUpdate = blogdataservice.GetEntry(entry.EntryId);
+			Assert.True(entryAfterUpdate.Title == "Test Entry 2");
+
+			blogdataservice.DeleteEntry(entry.EntryId, null);
+		}
+
+		[Theory]
+		[MemberData(nameof(DasBlogDataService))]
+		[Trait("Category", "UnitTest")]
+		public void BlogDataService_GetEntriesForDay_Successful(IBlogDataService blogdataservice)
+		{
+			var entries = blogdataservice.GetEntriesForDay(DateTime.MaxValue.AddDays(-2), DateTimeZone.Utc, string.Empty, int.MaxValue, int.MaxValue, string.Empty);
+			Assert.NotNull(entries);
+		}
+
+		[Theory]
+		[MemberData(nameof(DasBlogDataService))]
+		[Trait("Category", "UnitTest")]
+		public void BlogDataService_SearchEntries_Successful(IBlogDataService blogdataservice)
 		{
 			var entries = blogdataservice.GetEntriesForDay(DateTime.MaxValue.AddDays(-2), DateTimeZone.Utc, string.Empty, int.MaxValue, int.MaxValue, string.Empty);
 			Assert.NotNull(entries);
@@ -30,7 +90,5 @@ namespace DasBlog.Tests.UnitTests.Services
 		{
 			BlogDataServiceFactory.GetService(UnitTestsConstants.TestContentLocation, LoggingDataServiceFactory.GetService(UnitTestsConstants.TestLoggingLocation))
 		};
-
-
 	}
 }
