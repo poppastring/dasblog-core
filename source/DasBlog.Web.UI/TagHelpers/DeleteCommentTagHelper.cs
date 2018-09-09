ce0a0152b8ba3de533d4da562dab82b59aba773c
Mark Downie <poppastring@users.noreply.github.com>
2018-09-08 23:47:13 -0400

    Created new taghelpers for Comment approval and deletion. Embed them in the partial view within an authorization tag.

---
+++ source/DasBlog.Web.UI/TagHelpers/DeleteCommentTagHelper.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using DasBlog.Core;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace DasBlog.Web.TagHelpers
+{
+    public class DeleteCommentTagHelper : TagHelper
+	{
+		public string BlogPostId { get; set; }
+
+		public string CommentId { get; set; }
+
+		public string CommentorName { get; set; }
+
+		private readonly IDasBlogSettings dasBlogSettings;
+
+		public DeleteCommentTagHelper(IDasBlogSettings dasBlogSettings)
+		{
+			this.dasBlogSettings = dasBlogSettings;
+		}
+
+		public override void Process(TagHelperContext context, TagHelperOutput output)
+		{
+			output.TagName = "a";
+			output.TagMode = TagMode.StartTagAndEndTag;
+			output.Attributes.SetAttribute("href", $"javascript:deleteComment(\"{BlogPostId}\",\"{CommentId}\",\"{CommentorName}\")");
+			output.Attributes.SetAttribute("class", "dbc-comment-delete-link");
+			output.Content.SetHtmlContent("Delete this comment");
+		}
+
+		public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+		{
+			return Task.Run(() => Process(context, output));
+		}
+	}
+}
--- source/DasBlog.Web.UI/wwwroot/js/site.js
+++ source/DasBlog.Web.UI/wwwroot/js/site.js
@@ -1,9 +1,22 @@
-﻿// Write your JavaScript code.
+﻿
 function deleteEntry(entryId, entryTitle) {
     if (confirm("Are you sure you want to delete this item? \n\n" + entryTitle)) {
-        location.href = "/post/" + entryId + "/delete"
+        location.href = "post/" + entryId + "/delete"
     }
-}    
+}
+
+function deleteComment(entryId, commentId, commentorName) {
+    if (confirm("Are you sure you want to delete this comment? \n\n" + commentorName)) {
+        location.href = "post/" + entryId + "/comment/" + commentId
+    }
+}
+
+function approveComment(entryId, commentId, commentorName) {
+    if (confirm("Are you sure you want to approve this comment? \n\n" + commentorName)) {
+        location.href = "post/" + entryId + "/comment/" + commentId
+    }
+}
+
 function linkToUser(emailAddress, linkAbility) {
     if (linkAbility === "disabled") {
         alert("You must complete the current operation (create, edit or delete user), before you can select another");
