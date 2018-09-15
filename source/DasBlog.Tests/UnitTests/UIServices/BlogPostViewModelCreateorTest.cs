49c13f07e692458d22720c1994d3b0fbb514f552
mike <mikemay@blueyonder.co.uk>
2018-09-15 07:33:18 +0100

    struggling with moq

--- source/DasBlog.Tests/UnitTests/UIServices/BlogPostViewModelCreateorTest.cs
+++ source/DasBlog.Tests/UnitTests/UIServices/BlogPostViewModelCreateorTest.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using AutoMapper;
 using DasBlog.Managers;
@@ -6,9 +7,12 @@ using DasBlog.Managers.Interfaces;
 using DasBlog.Web.Models.BlogViewModels;
 using DasBlog.Web.Services;
 using DasBlog.Web.Services.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
 using newtelligence.DasBlog.Runtime;
 using Remotion.Linq.Parsing.Structure.IntermediateModel;
 using Xunit;
+using Moq;
+
 
 namespace DasBlog.Tests.UnitTests.UIServices
 {
@@ -19,9 +23,24 @@ namespace DasBlog.Tests.UnitTests.UIServices
 
 		public BlogPostViewModelCreateorTest()
 		{
-			this.blogManager = new FakeBlogManager();
-			this.mapper = new FakeMapper();
+			IServiceProvider serviceProvider = InjectDependencies();
+			var blogManagerMock = new Mock<IBlogManager>();
+			blogManagerMock.Setup(bm => bm.GetCategories())
+			  .Returns(new CategoryCacheEntryCollection());
+			this.blogManager = blogManagerMock.Object;
+			var mapperMock = new Mock<IMapper>();
+			mapperMock.Setup(m => m.Map<IBlogManager, IList<CategoryViewModel>>(this.blogManager))
+			  .Returns(new List<CategoryViewModel>{new CategoryViewModel()});
+			this.mapper = mapperMock.Object;
 		}
+
+		private IServiceProvider InjectDependencies()
+		{
+			var services = new ServiceCollection();
+			
+			return services.BuildServiceProvider();
+		}
+
 		[Fact]
 		[Trait("Category", "UnitTest")]
 		public void WhenCreated_DefaultBlogPost_IsPublic()
@@ -39,137 +58,13 @@ namespace DasBlog.Tests.UnitTests.UIServices
 			Assert.True(postViewModel.Languages.Count() > 50);
 				// 841 entries on Windows 10 UK english Imac Parallels Windows Version 10.0.17134.285]
 		}
-		[Fact(Skip = "true")]
+		[Fact(Skip="true")]
 		[Trait("Category", "UnitTest")]
 		public void WhenCreated_DefaultBlogPost_IncludesExistingCategories()
 		{
 			IBlogPostViewModelCreator bpvmc = new BlogPostViewModelCreator(blogManager, mapper);
 			var postViewModel = bpvmc.CreateBlogPostVN();
-			Assert.Equal(5, postViewModel.AllCategories.Count);
+			Assert.Equal(1, postViewModel.AllCategories.Count);
 		}
-	}
-
-	internal class FakeBlogManager : IBlogManager
-	{
-		public Entry GetBlogPost(string postid, DateTime? postDate)
-		{
-			throw new NotImplementedException();
-		}
-
-		public Entry GetEntryForEdit(string postid)
-		{
-			throw new NotImplementedException();
-		}
-
-		public EntryCollection GetFrontPagePosts(string acceptLanguageHeader)
-		{
-			throw new NotImplementedException();
-		}
-
-		public EntryCollection GetEntriesForPage(int pageIndex, string acceptLanguageHeader)
-		{
-			throw new NotImplementedException();
-		}
-
-		public EntrySaveState CreateEntry(Entry entry)
-		{
-			throw new NotImplementedException();
-		}
-
-		public EntrySaveState UpdateEntry(Entry entry)
-		{
-			throw new NotImplementedException();
-		}
-
-		public void DeleteEntry(string postid)
-		{
-			throw new NotImplementedException();
-		}
-
-		public CategoryCacheEntryCollection GetCategories()
-		{
-			throw new NotImplementedException();
-		}
-
-		public CommentSaveState AddComment(string postid, Comment comment)
-		{
-			throw new NotImplementedException();
-		}
-
-		public CommentSaveState DeleteComment(string postid, string commentid)
-		{
-			throw new NotImplementedException();
-		}
-
-		public CommentSaveState ApproveComment(string postid, string commentid)
-		{
-			throw new NotImplementedException();
-		}
-
-		public CommentCollection GetComments(string postid, bool allComments)
-		{
-			throw new NotImplementedException();
-		}
-
-		public EntryCollection SearchEntries(string searchString, string acceptLanguageHeader)
-		{
-			throw new NotImplementedException();
-		}
-	}
-
-	internal class FakeMapper : IMapper
-	{
-		public TDestination Map<TDestination>(object source)
-		{
-			throw new NotImplementedException();
-		}
-
-		public TDestination Map<TDestination>(object source, Action<IMappingOperationOptions> opts)
-		{
-			throw new NotImplementedException();
-		}
-
-		public TDestination Map<TSource, TDestination>(TSource source)
-		{
-			throw new NotImplementedException();
-		}
-
-		public TDestination Map<TSource, TDestination>(TSource source, Action<IMappingOperationOptions<TSource, TDestination>> opts)
-		{
-			throw new NotImplementedException();
-		}
-
-		public TDestination Map<TSource, TDestination>(TSource source, TDestination destination)
-		{
-			throw new NotImplementedException();
-		}
-
-		public TDestination Map<TSource, TDestination>(TSource source, TDestination destination, Action<IMappingOperationOptions<TSource, TDestination>> opts)
-		{
-			throw new NotImplementedException();
-		}
-
-		public object Map(object source, Type sourceType, Type destinationType)
-		{
-			throw new NotImplementedException();
-		}
-
-		public object Map(object source, Type sourceType, Type destinationType, Action<IMappingOperationOptions> opts)
-		{
-			throw new NotImplementedException();
-		}
-
-		public object Map(object source, object destination, Type sourceType, Type destinationType)
-		{
-			throw new NotImplementedException();
-		}
-
-		public object Map(object source, object destination, Type sourceType, Type destinationType, Action<IMappingOperationOptions> opts)
-		{
-			throw new NotImplementedException();
-		}
-
-		public IConfigurationProvider ConfigurationProvider { get; }
-		public Func<Type, object> ServiceCtor { get; }
 	}
 }
