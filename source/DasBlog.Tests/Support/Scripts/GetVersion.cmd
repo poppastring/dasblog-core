6c16091532545b38f28e71afc51ae7a46c737ddd
Mike May <mikemay@blueyonder.co.uk>
2018-10-23 08:58:41 +0100

    trying hard with AppVeyor

--- source/DasBlog.Tests/Support/Scripts/GetVersion.cmd
+++ source/DasBlog.Tests/Support/Scripts/GetVersion.cmd
@@ -6,6 +6,12 @@ rem # returns the version of GIT installed
 echo dasmeta %time% %0
 echo dasmeta_output_start
 echo dasmeta_errors_start
+git config user.email "mikemay@blueyonder.co.uk" 2>&1
+if errorlevel 1 goto err_exit
+git config user.naem "Mike May" 2>&1
+if errorlevel 1 goto err_exit
 git --version 2>&1
 echo dasmeta_output_complete errorlevel==%errorlevel%
+exit %errorlevel%
+:err_exit
 exit %errorlevel%
