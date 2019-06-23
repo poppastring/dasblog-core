f0ea4733cb7483ea2791de4ee9ee551bfabe04f5
Mark Downie <poppastring@users.noreply.github.com>
2019-06-22 22:17:42 -0400

    Microsummary fix
    Micro summary resolved.

--- source/DasBlog.Web.UI/Controllers/SiteController.cs
+++ source/DasBlog.Web.UI/Controllers/SiteController.cs
@@ -1,20 +1,30 @@
-﻿using DasBlog.Managers.Interfaces;
+﻿using System.Linq;
+using AutoMapper;
+using DasBlog.Managers.Interfaces;
+using DasBlog.Web.Models.BlogViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace DasBlog.Web.Controllers
 {
-    [Produces("text/xml")]
     [Route("site")]
     public class SiteController : DasBlogController
     {
-        private ISiteManager siteManager;
+        private readonly ISiteManager siteManager;
+		private readonly IBlogManager blogManager;
+		private readonly IMemoryCache memoryCache;
+		private readonly IMapper mapper;
 
-        public SiteController(ISiteManager siteManager)
+		public SiteController(ISiteManager siteManager, IBlogManager blogManager, IMemoryCache memoryCache, IMapper mapper)
         {
 			this.siteManager = siteManager;
-        }
+			this.blogManager = blogManager;
+			this.memoryCache = memoryCache;
+			this.mapper = mapper;
+		}
 
-        [Route("")]
+		[Produces("text/xml")]
+		[Route("")]
         [HttpGet("map")]
         public ActionResult Map()
         {
@@ -22,5 +32,24 @@ namespace DasBlog.Web.Controllers
 
             return Ok(sitemap);
         }
-    }
+
+		[Produces("text/plain")]
+		[HttpGet("microsummary")]
+		public ActionResult MicroSummary()
+		{
+			if (!memoryCache.TryGetValue(CACHEKEY_FRONTPAGE, out ListPostsViewModel lpvm))
+			{
+				lpvm = new ListPostsViewModel
+				{
+					Posts = blogManager.GetFrontPagePosts(Request.Headers["Accept-Language"])
+								.Select(entry => mapper.Map<PostViewModel>(entry))
+								.Select(editentry => editentry).ToList()
+				};
+
+				memoryCache.Set(CACHEKEY_FRONTPAGE, lpvm, SiteCacheSettings());
+			};
+
+			return Ok(lpvm?.Posts.FirstOrDefault()?.Title);
+		}
+	}
 }
