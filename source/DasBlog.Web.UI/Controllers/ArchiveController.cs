d71cb2250a34c759bcac6d1bd8e0b730e37d5c54
Scott Hanselman <scott@hanselman.com>
2019-07-03 00:38:02 -0700

    adding support for YEAR archives

--- source/DasBlog.Web.UI/Controllers/ArchiveController.cs
+++ source/DasBlog.Web.UI/Controllers/ArchiveController.cs
@@ -1,15 +1,19 @@
 ﻿using AutoMapper;
 using DasBlog.Managers.Interfaces;
 using DasBlog.Services;
+using System.Linq;
 using DasBlog.Web.Models.BlogViewModels;
 using DasBlog.Web.Settings;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using newtelligence.DasBlog.Runtime;
 using System;
+using System.Collections.Generic;
 
 namespace DasBlog.Web.Controllers
 {
 	[Route("archive")]
+	[ResponseCache(Duration = 14400, Location = ResponseCacheLocation.Any)]
 	public class ArchiveController : DasBlogBaseController
 	{
 		private IArchiveManager archiveManager;
@@ -35,7 +39,7 @@ namespace DasBlog.Web.Controllers
 		public IActionResult Archive(int year)
 		{
 			var dateTime = new DateTime(year, 1, 1);
-			var months = GetMonthsViewModel(dateTime);
+			var months = GetMonthsViewModel(dateTime, true);
 			return View(months);
 		}
 
@@ -50,19 +54,31 @@ namespace DasBlog.Web.Controllers
 		[HttpGet("{year}/{month}/{day}")]
 		public IActionResult Archive(int year, int month, int day)
 		{
-			DateTime dateTime = new DateTime(year, month, day);
+			var dateTime = new DateTime(year, month, day);
 			var months = GetMonthsViewModel(dateTime);
 			return View(months);
 		}
 
-		private MonthViewViewModel GetMonthsViewModel(DateTime dateTime)
+		private List<MonthViewViewModel> GetMonthsViewModel(DateTime dateTime, bool wholeYear = false)
 		{
 			string languageFilter = httpContextAccessor.HttpContext.Request.Headers["Accept-Language"];
 
 			ViewBag.PreviousMonth = dateTime.AddMonths(-1).Date;
 			ViewBag.NextMonth = dateTime.AddMonths(1).Date;
 			ViewBag.CurrentMonth = dateTime.Date;
-			var entries = archiveManager.GetEntriesForMonth(dateTime, languageFilter);
+
+			//unique list of years for the top of archives
+			var daysWithEntries = archiveManager.GetDaysWithEntries();
+			ViewBag.Years = daysWithEntries.Select(i => i.Year).Distinct();
+
+			EntryCollection entries;
+			if (wholeYear)
+				entries = archiveManager.GetEntriesForYear(dateTime, languageFilter);
+			else
+				entries = archiveManager.GetEntriesForMonth(dateTime, languageFilter);
+
+			//TODO: Do I need this?
+			//entries = new EntryCollection(entries.OrderBy(e => e.CreatedUtc));
 
 			DefaultPage(ARCHIVE);
 			return MonthViewViewModel.Create(dateTime, entries, mapper);
