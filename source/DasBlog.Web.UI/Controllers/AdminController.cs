e9be8ff4cba01498fa30f1647dbc7a8fcd982fed
Mark Downie <poppastring@users.noreply.github.com>
2019-11-24 18:53:41 -0500

    Initial check ino

---
+++ source/DasBlog.Web.UI/Controllers/AdminController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DasBlog.Managers.Interfaces;
+using DasBlog.Services;
+using DasBlog.Web.Settings;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DasBlog.Web.Controllers
+{
+	[Authorize]
+	public class AdminController : DasBlogBaseController
+	{
+		private readonly IDasBlogSettings dasBlogSettings;
+
+		public AdminController(IDasBlogSettings dasBlogSettings) : base(dasBlogSettings)
+		{
+			this.dasBlogSettings = dasBlogSettings;
+		}
+
+		public IActionResult Index()
+        {
+            return View();
+        }
+
+		[HttpGet]
+		public IActionResult Settings()
+		{
+			return View();
+		}
+    }
+}
