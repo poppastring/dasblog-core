98c9029b49d28ad9f7fa6d36c1bc14f127a55945
Mark Downie <poppastring@users.noreply.github.com>
2019-12-21 23:48:52 -0500

    Added new view models to allow for lists in the settings page.

---
+++ source/DasBlog.Web.UI/Models/AdminViewModels/TimeZoneIndexViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DasBlog.Web.Models.AdminViewModels
+{
+	public class TimeZoneIndexViewModel
+	{
+		public string Name { get; set; }
+
+		public int Id { get; set; }
+
+		public List<TimeZoneIndexViewModel> Init()
+		{
+			return new List<TimeZoneIndexViewModel>() {
+				new TimeZoneIndexViewModel { Id = 0, Name = "" },
+				new TimeZoneIndexViewModel { Id = 1, Name = "" },
+			};
+		}
+	}
+}
---
+++ source/DasBlog.Web.UI/Models/AdminViewModels/TwitterCardViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DasBlog.Web.Models.AdminViewModels
+{
+	public class TwitterCardViewModel
+	{
+		public string Name { get; set; }
+
+		public string Id { get; set; }
+
+		public List<TwitterCardViewModel> Init()
+		{
+			return new List<TwitterCardViewModel>() {
+				new TwitterCardViewModel { Id = "summarywithlargeimage", Name = "Summary card with a large image" },
+				new TwitterCardViewModel { Id = "summary", Name = "Summary card" }
+			};
+		}
+	}
+}
