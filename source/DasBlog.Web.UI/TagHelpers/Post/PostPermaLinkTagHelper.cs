9ec5ab40b3ebfd73ca14ffbbc6bf1e5c6077af23
Mark Downie <poppastring@users.noreply.github.com>
2020-02-08 13:59:53 -0500

    Update default BlogItem with new taghelpers

---
+++ source/DasBlog.Web.UI/TagHelpers/Post/PostPermaLinkTagHelper.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading.Tasks;
+using DasBlog.Services;
+using DasBlog.Web.Models.BlogViewModels;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace DasBlog.Web.TagHelpers.Post
+{
+	public class PostPermaLinkTagHelper : TagHelper
+	{
+		public PostViewModel Post { get; set; }
+
+		public string Css { get; set; }
+
+		private readonly IDasBlogSettings dasBlogSettings;
+
+		public PostPermaLinkTagHelper(IDasBlogSettings dasBlogSettings)
+		{
+			this.dasBlogSettings = dasBlogSettings;
+		}
+
+		public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+		{
+			var linkvalue = Post.Title;
+			output.TagMode = TagMode.StartTagAndEndTag;
+			output.TagName = "a";
+
+			var content = await output.GetChildContentAsync();
+
+			if(string.IsNullOrWhiteSpace(content.GetContent()))
+			{
+				linkvalue = content.GetContent();
+			}
+
+			if (!string.IsNullOrEmpty(Css))
+			{
+				output.Attributes.SetAttribute("class", Css);
+			}
+
+			output.Attributes.SetAttribute("href", dasBlogSettings.RelativeToRoot(Post.PermaLink));
+			output.Content.SetHtmlContent(linkvalue);
+		}
+
+	}
+}
