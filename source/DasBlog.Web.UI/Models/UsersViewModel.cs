4c9dd4b0ba22d2e7741fad146a920c2555af06a6
Brian Bagley <Brian.Bagley@lubrizol.com>
2020-02-22 20:22:00 -0500

    Changed dipslay name to display name on the Users view model.

--- source/DasBlog.Web.UI/Models/UsersViewModel.cs
+++ source/DasBlog.Web.UI/Models/UsersViewModel.cs
@@ -3,48 +3,48 @@ using DasBlog.Core.Security;
 
 namespace DasBlog.Web.Models
 {
-	public class UsersViewModel
-	{		
-		public string Name { get; set; }
+    public class UsersViewModel
+    {
+        public string Name { get; set; }
 
-		[Required]
-		public Role Role { get; set; }
-		
-		[Required]
-		[EmailAddress]
-		[Display(Name="Email Address")]
-		public string EmailAddress { get; set; }
+        [Required]
+        public Role Role { get; set; }
 
-		[Required]
-		[Display(Name= "Dipslay Name")]
-		[StringLength(50, ErrorMessage = "Invalid Display Name length", MinimumLength = 1)]
-		public string DisplayName { get; set; }
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email Address")]
+        public string EmailAddress { get; set; }
 
-		[Display(Name= "Open ID Url")]
-		public string OpenIDUrl { get; set; }
+        [Required]
+        [Display(Name = "Display Name")]
+        [StringLength(50, ErrorMessage = "Invalid Display Name length", MinimumLength = 1)]
+        public string DisplayName { get; set; }
 
-		[Display(Name= "Receive Notifications For A New Post")]
-		public bool NotifyOnNewPost { get; set; }
+        [Display(Name = "Open ID Url")]
+        public string OpenIDUrl { get; set; }
 
-		[Display(Name= "Receive Notifications for All Comments")]
-		public bool NotifyOnAllComment { get; set; }
+        [Display(Name = "Receive Notifications For A New Post")]
+        public bool NotifyOnNewPost { get; set; }
 
-		[Display(Name= "Receive Notifications for Own Comments")]
-		public bool NotifyOnOwnComment { get; set; }
+        [Display(Name = "Receive Notifications for All Comments")]
+        public bool NotifyOnAllComment { get; set; }
 
-		[Required]
-		[Display(Name = "Password")]
-		[StringLength(100, ErrorMessage = "Invalid Password length", MinimumLength = 1)]
-		public string Password { get; set; }
+        [Display(Name = "Receive Notifications for Own Comments")]
+        public bool NotifyOnOwnComment { get; set; }
 
-		[Required]
-		public bool Active { get; set; }
+        [Required]
+        [Display(Name = "Password")]
+        [StringLength(100, ErrorMessage = "Invalid Password length", MinimumLength = 1)]
+        public string Password { get; set; }
 
-		[Required]
-		public bool Ask { get; set; }
+        [Required]
+        public bool Active { get; set; }
 
-		public string Writability { get; set; }
+        [Required]
+        public bool Ask { get; set; }
 
-		public string OriginalEmail { get; set; }
-	}
+        public string Writability { get; set; }
+
+        public string OriginalEmail { get; set; }
+    }
 }
