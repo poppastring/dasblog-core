05c03a6c1e676db7653e636e5d882fd99aa1ae9f
Mark Downie <poppastring@users.noreply.github.com>
2020-02-25 22:19:58 -0500

    Putting the basics in place with successful builds.

---
+++ source/DasBlog.Tests/DasBlog.Test.Integration/SeleniumServerFactory.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using DasBlog.Web;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+
+namespace DasBlog.Test.Integration
+{
+	public class SeleniumServerFactory<TStartup> : WebApplicationFactory<Startup> where TStartup : class
+	{
+		IWebHost _host;
+		public string RootUri { get; set; }
+		Process _process;
+
+		public SeleniumServerFactory()
+		{
+			if (AreWe.InDockerOrBuildServer) return;
+
+			ClientOptions.BaseAddress = new Uri("https://localhost"); //will follow redirects by default
+
+			_process = new Process()
+			{
+				StartInfo = new ProcessStartInfo
+				{
+					FileName = "selenium-standalone",
+					Arguments = "start",
+					UseShellExecute = true,
+					CreateNoWindow = false
+				}
+			};
+			_process.Start();
+		}
+
+		// protected override void ConfigureWebHost(IWebHostBuilder builder)
+		// {
+		//     builder.UseEnvironment("Development"); //will be default in RC1
+		//     //builder.UseKestrel();
+		// }
+
+		protected override TestServer CreateServer(IWebHostBuilder builder)
+		{
+			//Real TCP port
+			_host = builder.Build();
+			_host.Start();
+			RootUri = _host.ServerFeatures.Get<IServerAddressesFeature>().Addresses.LastOrDefault(); //Last is ssl!
+
+			//Fake Server we won't use
+			return new TestServer(new WebHostBuilder().UseStartup<TStartup>());
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			base.Dispose(disposing);
+			if (disposing)
+			{
+				_host?.Dispose();
+				_process?.CloseMainWindow();
+			}
+		}
+	}
+}
---
+++ source/DasBlog.Tests/DasBlog.Test.Integration/SeleniumTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Net.Http;
+using DasBlog.Web;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using Xunit;
+
+namespace DasBlog.Test.Integration
+{
+	[Trait("Category", "SkipWhenLiveUnitTesting")]
+	public class SeleniumTests : IClassFixture<SeleniumServerFactory<Startup>>, IDisposable
+	{
+		public HttpClient Client { get; }
+		public SeleniumServerFactory<Startup> Server { get; }
+		public IWebDriver Browser { get; }
+		public ILogs Logs { get; }
+
+		public SeleniumTests(SeleniumServerFactory<Startup> server)
+		{
+			Console.WriteLine("In Docker?" + AreWe.InDockerOrBuildServer);
+			if (AreWe.InDockerOrBuildServer) return;
+			Server = server;
+			Client = server.CreateClient(); //weird side effecty thing here. This shouldn't be required but it is.
+
+			var opts = new ChromeOptions();
+			//opts.AddArgument("--headless");
+			opts.SetLoggingPreference(OpenQA.Selenium.LogType.Browser, LogLevel.All);
+
+			var driver = new RemoteWebDriver(opts);
+			Browser = driver;
+			Logs = new RemoteLogs(driver); //TODO: Still not bringing the logs over yet?
+		}
+
+		[SkippableFact(typeof(OpenQA.Selenium.WebDriverException))]
+		public void LoadTheMainPageAndCheckTitle()
+		{
+			Skip.If(AreWe.InDockerOrBuildServer);
+			Browser.Navigate().GoToUrl(Server.RootUri);
+			Assert.StartsWith("Hanselminutes Technology Podcast - Fresh Air and Fresh Perspectives for Developers", Browser.Title);
+		}
+
+		[SkippableFact(typeof(OpenQA.Selenium.WebDriverException))]
+		public void ThereIsAnH1()
+		{
+			Skip.If(AreWe.InDockerOrBuildServer);
+			Browser.Navigate().GoToUrl(Server.RootUri);
+
+			var headerSelector = By.TagName("h1");
+			Assert.Equal("HANSELMINUTES PODCAST by Scott Hanselman", Browser.FindElement(headerSelector).Text);
+		}
+
+		[SkippableFact(typeof(OpenQA.Selenium.WebDriverException))]
+		public void KevinScottTest()
+		{
+			Skip.If(AreWe.InDockerOrBuildServer);
+			Browser.Navigate().GoToUrl(Server.RootUri + "/631/how-do-you-become-a-cto-with-microsofts-cto-kevin-scott");
+
+			var headerSelector = By.TagName("h2");
+			Assert.Equal("How do you become a CTO - with Microsoft's CTO Kevin Scott", Browser.FindElement(headerSelector).Text);
+		}
+
+		[SkippableFact(typeof(OpenQA.Selenium.WebDriverException))]
+		public void KevinScottTestThenGoHome()
+		{
+			Skip.If(AreWe.InDockerOrBuildServer, "In Docker!");
+			Browser.Navigate().GoToUrl(Server.RootUri + "/631/how-do-you-become-a-cto-with-microsofts-cto-kevin-scott");
+
+			var headerSelector = By.TagName("h1");
+			var link = Browser.FindElement(headerSelector);
+			link.Click();
+			Assert.Equal(Browser.Url.TrimEnd('/'), Server.RootUri); //WTF
+		}
+
+		public void Dispose()
+		{
+			if (Browser != null)
+				Browser.Dispose();
+		}
+	}
+}
