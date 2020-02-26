8c3893f325d37052685ee291ac0c89a143c15b3f
Mark Downie <poppastring@users.noreply.github.com>
2020-02-25 21:47:03 -0500

    New integration project created.

---
+++ source/DasBlog.Tests/DasBlog.Test.Integration/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace DasBlog.Test.Integration
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
