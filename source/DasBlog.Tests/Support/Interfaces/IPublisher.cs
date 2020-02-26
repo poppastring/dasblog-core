3366e76826d23218a88feb6a57cb994154dc507f
Mark Downie <poppastring@users.noreply.github.com>
2020-02-25 21:26:33 -0500

    Removed one more project....

--- source/DasBlog.Tests/Support/Interfaces/IPublisher.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace DasBlog.Tests.Support.Interfaces
-{
-	public interface IPublisher
-	{
-		void Publish(IEnumerable<TestResults.Result> results);
-	}
-}
