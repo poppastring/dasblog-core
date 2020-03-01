5c5da618e0b5808d109007bb84651395af6956fe
Mark Downie <poppastring@users.noreply.github.com>
2020-02-29 23:00:01 -0500

    A comprehensive list of integration tests, still needs a little refactoring, one still fails and I cannot quite figure out why.

--- source/DasBlog.Tests/DasBlog.Test.Integration/SeleniumPageTests.cs
+++ source/DasBlog.Tests/DasBlog.Test.Integration/SeleniumPageTests.cs
@@ -111,11 +111,11 @@ namespace DasBlog.Test.Integration
 		public void NavigateToSpecificCategoryNavigateToPost()
 		{
 			Skip.If(AreWe.InDockerOrBuildServer, "In Docker!");
-			Browser.Navigate().GoToUrl(Server.RootUri + "/category/dasblogcore");
+			Browser.Navigate().GoToUrl(Server.RootUri + "/category/dasblog-core");
 			Assert.StartsWith("Category - My DasBlog!", Browser.Title);
 
 			var headerSelector = By.TagName("h4");
-			Assert.Equal("dasblog-core (1)", Browser.FindElement(headerSelector).Text);
+			Assert.Equal("Dasblog Core (1)", Browser.FindElement(headerSelector).Text);
 
 			var titleSelector = By.LinkText("Welcome to DasBlog Core");
 			Browser.FindElement(titleSelector).Click();
@@ -166,22 +166,220 @@ namespace DasBlog.Test.Integration
 			Assert.Equal(Browser.Url.TrimEnd('/'), Server.RootUri + "/archive/2020/3");
 		}
 
+		[SkippableFact(typeof(OpenQA.Selenium.WebDriverException))]
+		public void NavigateToRSSFeed()
+		{
+			Skip.If(AreWe.InDockerOrBuildServer, "In Docker!");
 
-		// Create Comments
+			Browser.Navigate().GoToUrl(Server.RootUri + "/feed/rss");
 
-		// Login
+			Assert.Equal(Browser.Url.TrimEnd('/'), Server.RootUri + "/feed/rss");
+		}
+
+		[SkippableFact(typeof(OpenQA.Selenium.WebDriverException))]
+		public void NavigateToPostAndCreateComment()
+		{
+			Skip.If(AreWe.InDockerOrBuildServer, "In Docker!");
+
+			const string commentname = "First Name";
+
+			Browser.Navigate().GoToUrl(Server.RootUri + "/welcome-to-dasblog-core");
+
+			SendKeysToElement("Name", commentname);
+
+			SendKeysToElement("Email", "someemail@someplace.com");
+
+			SendKeysToElement("HomePage", "https://www.github.com/poppastring/dasblog-core");
+
+			SendKeysToElement("CheesyQuestionAnswered", "7");
+
+			SendKeysToElement("Content", "A comment about this blog post");
+			
+			var navSelector = By.Id("SaveContentButton");
+			var link = Browser.FindElement(navSelector);
+			link.Click();
+
+			Assert.Equal(Browser.Url.TrimEnd('/'), Server.RootUri + "/welcome-to-dasblog-core/comments#comments-start");
+
+			var elementid = By.ClassName("dbc-comment-user-homepage-name");
+			Assert.Equal(commentname, Browser.FindElement(elementid).Text);
+		}
+
 
 		// Approve Comment
-
 		// Delete Comment
 
-		// Create category
+		[SkippableFact(typeof(OpenQA.Selenium.WebDriverException))]
+		public void NavigateToPostAndCreateCommentApproveCommentDeleteComment()
+		{
+			NavigateToPostAndCreateComment();
 
-		// Create Post
+			LoginToSite();
 
-		// Edit Post
+			Browser.Navigate().GoToUrl(Server.RootUri + "/welcome-to-dasblog-core");
 
-		// Delete Post
+			var approveSelector = By.LinkText("Approve this comment");
+			var approvelink = Browser.FindElement(approveSelector);
+			approvelink.Click();
+
+			Browser.SwitchTo().Alert().Accept();
+
+			// Check the comment is approved
+			Browser.Navigate().GoToUrl(Server.RootUri + "/welcome-to-dasblog-core");
+
+			var approvedSelector = By.LinkText("Comment Approved");
+			var approvedlink = Browser.FindElement(approvedSelector);
+
+			// Delete this comment
+			Browser.Navigate().GoToUrl(Server.RootUri + "/welcome-to-dasblog-core");
+
+			var deleteSelector = By.LinkText("Delete this comment");
+			var deletelink = Browser.FindElement(deleteSelector);
+			deletelink.Click();
+
+			Browser.SwitchTo().Alert().Accept();
+
+			try
+			{
+				Browser.Navigate().GoToUrl(Server.RootUri + "/welcome-to-dasblog-core");
+
+				deleteSelector = By.LinkText("Delete this comment");
+				deletelink = Browser.FindElement(deleteSelector);
+			}
+			catch (Exception ex)
+			{
+				Assert.StartsWith("Welcome to DasBlog Core", Browser.Title);
+			}
+
+		}
+
+
+
+		[SkippableFact(typeof(OpenQA.Selenium.WebDriverException))]
+		public void NavigateToLoginPageLoginThenLogout()
+		{
+			Skip.If(AreWe.InDockerOrBuildServer, "In Docker!");
+
+			LoginToSite();
+
+			var createpostSelector = By.Id("CreatePostLink");
+			var createpostlink = Browser.FindElement(createpostSelector);
+			createpostlink.Click();
+
+			Assert.Equal(Browser.Url.TrimEnd('/'), Server.RootUri + "/post/create");
+
+			Browser.Navigate().GoToUrl(Server.RootUri + "/account/logout");
+
+			try
+			{
+				createpostSelector = By.Id("CreatePostLink");
+				createpostlink = Browser.FindElement(createpostSelector);
+			}
+			catch(Exception ex)
+			{
+				Assert.StartsWith("My DasBlog!", Browser.Title);
+			}
+		}
+
+		[SkippableFact(typeof(OpenQA.Selenium.WebDriverException))]
+		public void LoginCreateAPostThenEditPostThenDeletAPost()
+		{
+			Skip.If(AreWe.InDockerOrBuildServer, "In Docker!");
+
+			LoginToSite();
+
+			// Fill out post
+			Browser.Navigate().GoToUrl(Server.RootUri + "/post/create");
+
+			SendKeysToElement("BlogTitle", "A New Post");
+
+			SendKeysToElement("mytextarea", "We certainly hope this works...");
+
+			SendKeysToElement("BlogNewCategoryName", "Test Category");
+
+			// Add a new category
+			var newCategorySelector = By.Id("NewCategorySubmit");
+			var newcategorylink = Browser.FindElement(newCategorySelector);
+			newcategorylink.Click();
+
+			Assert.Equal(Browser.Url.TrimEnd('/'), Server.RootUri + "/post/create");
+
+			var blogpostSelector = By.Id("BlogPostCreateSubmit");
+			var blogpostSubmitlink = Browser.FindElement(blogpostSelector);
+			blogpostSubmitlink.Click();
+
+			// Check new post
+			Browser.Navigate().GoToUrl(Server.RootUri + "/a-new-post");
+
+			Assert.StartsWith("A New Post", Browser.Title);
+
+			// Check new category
+			Browser.Navigate().GoToUrl(Server.RootUri + "/category/test-category");
+			var titleSelector = By.LinkText("A New Post");
+			Browser.FindElement(titleSelector).Click();
+
+			Assert.StartsWith("A New Post", Browser.Title);
+
+		
+			//Edit the post title
+			var editpostSelector = By.LinkText("Edit this post");
+			var editpostSubmitlink = Browser.FindElement(editpostSelector);
+			editpostSubmitlink.Click();
+
+			SendKeysToElement("BlogTitle", " Now Edit");
+			blogpostSelector = By.Id("BlogPostEditSubmit");
+			blogpostSubmitlink = Browser.FindElement(blogpostSelector);
+			blogpostSubmitlink.Click();
+
+			//Check the new post title
+			Browser.Navigate().GoToUrl(Server.RootUri + "/a-new-post-now-edit");
+			Assert.StartsWith("A New Post Now Edit", Browser.Title);
+
+			//Delete this post
+			var deletepostSelector = By.LinkText("Delete this post");
+			var deletepostSubmitlink = Browser.FindElement(deletepostSelector);
+			deletepostSubmitlink.Click();
+
+			Browser.SwitchTo().Alert().Accept();
+
+			// logout
+			Browser.Navigate().GoToUrl(Server.RootUri + "/account/logout");
+
+			try
+			{
+				var titledeleteSelector = By.LinkText("A New Post Now Edit");
+				Browser.FindElement(titledeleteSelector).Click();
+			}
+			catch (Exception ex)
+			{
+				Assert.StartsWith("My DasBlog!", Browser.Title);
+			}
+		}
+
+		// Site Admin
+		// Users
+		//	Activity
+
+
+		private void LoginToSite()
+		{
+			Browser.Navigate().GoToUrl(Server.RootUri + "/account/login");
+
+			SendKeysToElement("Email", "myemail@myemail.com");
+
+			SendKeysToElement("Password", "admin");
+
+			var navSelector = By.Id("LoginButton");
+			var link = Browser.FindElement(navSelector);
+			link.Click();
+		}
+
+		private void SendKeysToElement(string element, string keystosend)
+		{
+			var elementid = By.Id(element);
+			var eleementlink = Browser.FindElement(elementid);
+			eleementlink.SendKeys(keystosend);
+		}
 
 
 		public void Dispose()
