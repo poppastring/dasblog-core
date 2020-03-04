e7313929d840bb5dd4d3315e95b4eb49b4d571ca
Mark Downie <poppastring@users.noreply.github.com>
2020-03-03 22:30:37 -0500

    Notes on what I am trying to accomplish

--- source/DasBlog.CLI/Program.cs
+++ source/DasBlog.CLI/Program.cs
@@ -4,9 +4,34 @@ namespace DasBlog.CLI
 {
     class Program
     {
-        static void Main(string[] args)
+		public static string ASPNETCORE_ENVIRONMENT = string.Empty;
+		public const string ADMINPASSWORD = "19-A2-85-41-44-B6-3A-8F-76-17-A6-F2-25-01-9B-12";
+
+		static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+			if (args.Length == 0)
+			{
+				return;
+			}
+
+			ASPNETCORE_ENVIRONMENT = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+			
+			var command = args[0];
+
+			// "help" (list all commends)
+			// "help initialize" (explanation of initialize command)
+			// "initialize" (creates config files base on environment)
+			// "environment" (Lists the environment variable; configured directories; config file locations; root, theme, admin, list of themes);
+			// "root https://www.poppastring.com/blog" (set root config setting)
+			// "theme fulcrum" (set theme setting)
+			// "contentdir content"
+			// "binarydir content/binary"
+			// "logdir logs"
+			// "resetpassword" (reset admin password)
+			// "newtheme business" (creates a new theme folder copied from dasblog)
+
+
+			Console.WriteLine("Hello World!");
         }
     }
 }
