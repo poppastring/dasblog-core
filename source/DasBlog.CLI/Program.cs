686f69d16ed1b89c205920ff68ff87b39dd1a09b
Mark Downie <poppastring@users.noreply.github.com>
2020-03-04 22:09:56 -0500

    Using a really cool nuget package to help configure the structure of the CLI. Now I just need to tie this into the main application.

--- source/DasBlog.CLI/Program.cs
+++ source/DasBlog.CLI/Program.cs
@@ -1,37 +1,145 @@
 ﻿using System;
+using McMaster.Extensions.CommandLineUtils;
 
 namespace DasBlog.CLI
 {
     class Program
     {
-		public static string ASPNETCORE_ENVIRONMENT = string.Empty;
+		public static string ASPNETCORE_ENVIRONMENT = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 		public const string ADMINPASSWORD = "19-A2-85-41-44-B6-3A-8F-76-17-A6-F2-25-01-9B-12";
 
-		static void Main(string[] args)
+		static int Main(string[] args)
         {
-			if (args.Length == 0)
+
+			var app = new CommandLineApplication
 			{
-				return;
-			}
+				Name = "dasblog-core",
+				Description = "Configure DasBlog Core from the CLI.",
+			};
 
-			ASPNETCORE_ENVIRONMENT = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-			
-			var command = args[0];
+			app.HelpOption(inherited: true);
+			app.Command("config", configCmd =>
+			{
+				configCmd.Description = "Allows updates to the primary configuration settings";
 
-			// "help" (list all commends)
-			// "help initialize" (explanation of initialize command)
-			// "initialize" (creates config files base on environment)
-			// "environment" (Lists the environment variable; configured directories; config file locations; root, theme, admin, list of themes);
-			// "root https://www.poppastring.com/blog" (set root config setting)
-			// "theme fulcrum" (set theme setting)
-			// "contentdir content"
-			// "binarydir content/binary"
-			// "logdir logs"
-			// "resetpassword" (reset admin password)
-			// "newtheme business" (creates a new theme folder copied from dasblog)
+				configCmd.OnExecute(() =>
+				{
+					Console.WriteLine("Specify a subcommand");
+					configCmd.ShowHelp();
+					return 1;
+				});
+
+				configCmd.Command("root", setCmd =>
+				{
+					setCmd.Description = "Required: Set the base URL value for the site e.g. http://www.somesite.com";
+					var val = setCmd.Argument("value", "Value of 'root' parameter").IsRequired();
+					setCmd.OnExecute(() =>
+					{
+						// Execute command
+						Console.WriteLine($"Site 'root' has been set to '{val.Value}', restart site for operation to take effect");
+					});
+				});
+
+				configCmd.Command("theme", setCmd =>
+				{
+					setCmd.Description = "Change the site theme";
+					var val = setCmd.Argument("value", "Value of 'theme' parameter").IsRequired();
+					setCmd.OnExecute(() =>
+					{
+						// Execute command
+						Console.WriteLine($"Site 'theme' has been set to '{val.Value}', restart site for operation to take effect");
+					});
+				});
+
+				configCmd.Command("contentdir", setCmd =>
+				{
+					setCmd.Description = "Change the site content directory location";
+					var val = setCmd.Argument("value", "Value of 'contentdir' parameter").IsRequired();
+					setCmd.OnExecute(() =>
+					{
+						// Execute command
+						Console.WriteLine($"Site 'contentdir' has been set to '{val.Value}', restart site for operation to take effect");
+					});
+				});
+
+				configCmd.Command("binarydir", setCmd =>
+				{
+					setCmd.Description = "Change the site binary directory location";
+					var val = setCmd.Argument("value", "Value of 'binarydir' parameter").IsRequired();
+					setCmd.OnExecute(() =>
+					{
+						// Execute command
+						Console.WriteLine($"Site 'binarydir' has been set to '{val.Value}', restart site for operation to take effect");
+					});
+				});
+
+				configCmd.Command("logdir", setCmd =>
+				{
+					setCmd.Description = "Change the site logd directory location";
+					var val = setCmd.Argument("value", "Value of 'logdir' parameter").IsRequired();
+					setCmd.OnExecute(() =>
+					{
+						// Execute command
+						Console.WriteLine($"Site 'logdir' has been set to '{val.Value}', restart site for operation to take effect");
+					});
+				});
+			});
+
+			app.Command("initialize", initCmd =>
+			{
+				initCmd.Description = "Initializing the site creates environment specific config files (Staging or Production)";
+				initCmd.OnExecute(() =>
+				{
+
+					// Execute command
+					Console.WriteLine("dasblog settings have been been initialized for this site");
+				});
+			});
+
+			app.Command("environment", envCmd =>
+			{
+				envCmd.Description = "List the main environment settings associated with DasBlog Core";
+				envCmd.OnExecute(() =>
+				{
+
+					// Get environment data 
+					Console.WriteLine("List environemnt data");
+				});
+
+			});
+
+			app.Command("resetpassword", resetCmd =>
+			{
+				resetCmd.Description = "Resets the admin password to 'admin'";
+				resetCmd.OnExecute(() =>
+				{
+
+					// Get environment data 
+					Console.WriteLine("List environemnt data");
+				});
+
+			});
+
+			app.Command("createtheme", createthemeCmd =>
+			{
+				createthemeCmd.Description = "Creates a new theme based on the default dasblog theme";
+				var val = createthemeCmd.Argument("value", "Name of the new theme").IsRequired();
+				createthemeCmd.OnExecute(() =>
+				{
+					// Execute command
+					Console.WriteLine($"New theme created: '{val.Value}' ");
+				});
+			});
 
 
-			Console.WriteLine("Hello World!");
-        }
+			app.OnExecute(() =>
+			{
+				Console.WriteLine("Specify a subcommand");
+				app.ShowHelp();
+				return 1;
+			});
+
+			return app.Execute(args);
+		}
     }
 }
