f1d04892fe24a6c7b2303c3b9f62cdd045af0765
Scott Hanselman <scott@hanselman.com>
2020-10-10 16:32:52 -0700

    application insights

--- source/DasBlog.Web.UI/Startup.cs
+++ source/DasBlog.Web.UI/Startup.cs
@@ -288,7 +288,8 @@ namespace DasBlog.Web
 			{
 				options.WaitForJobsToComplete = true;
 			});
-		}
+            services.AddApplicationInsightsTelemetry(Configuration["APPINSIGHTS_INSTRUMENTATIONKEY"]);
+        }
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IDasBlogSettings dasBlogSettings)
