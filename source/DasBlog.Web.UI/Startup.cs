025b96787d4cfb61655e5dcee3337bfce4ce38d0
Paul Hatcher <paulh@atrios.co.uk>
2020-11-24 18:55:37 +0000

    Update to AppInsight 2.15 and change startup to preferred method

--- source/DasBlog.Web.UI/Startup.cs
+++ source/DasBlog.Web.UI/Startup.cs
@@ -97,6 +97,8 @@ namespace DasBlog.Web
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddApplicationInsightsTelemetry();
+
 			services.AddLogging(builder =>
 			{
 				builder.AddFile(opts => opts.LogDirectory = LogFolderPath);
@@ -283,7 +285,6 @@ namespace DasBlog.Web
 			{
 				options.WaitForJobsToComplete = true;
 			});
-            services.AddApplicationInsightsTelemetry(Configuration["APPINSIGHTS_INSTRUMENTATIONKEY"]);
         }
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
