c3ed56e51fc6c1e687a0419d8cfbb7a8b3f29a79
Thais Damasceno <85142451+tsdamas@users.noreply.github.com>
2022-04-18 16:39:29 -0700

    Transfering code
    - Deleting code from DasBlogAll;
    - Adding code for review to .../PostReadTimeTagHelper file;

--- source/DasBlog.Web.UI/TagHelpers/Post/PostReadTimeTagHelper.cs
+++ source/DasBlog.Web.UI/TagHelpers/Post/PostReadTimeTagHelper.cs
@@ -20,8 +20,18 @@ namespace DasBlog.Web.TagHelpers.Post
 
 		public override void Process(TagHelperContext context, TagHelperOutput output)
 		{
+
+			string pattern = @"<img.*?src="".*?"".*?>";
+			Regex rx = new Regex(pattern);
+			var numberImages = string.Empty;
+			foreach(Match m in rx.Matches(Post.Content))
+			{
+				var numberImages = m.Count;
+			}
+
+			var imgMinutes = (numberImages*30)/60; 
 			var delimiters = new char[] { ' ', '\r', '\n' };
-			var minute = Math.Round((double)dasBlogSettings.FilterHtml(Post.Content).Split(delimiters, StringSplitOptions.RemoveEmptyEntries).Length / 200);
+			var minute = Math.Round((double)dasBlogSettings.FilterHtml(Post.Content).Split(delimiters, StringSplitOptions.RemoveEmptyEntries).Length / 200) + imgMinutes;
 
 			output.TagName = "span";
 			output.TagMode = TagMode.StartTagAndEndTag;
