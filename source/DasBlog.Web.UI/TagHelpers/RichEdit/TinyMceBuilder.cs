a28dd8d8db727d9f57f78a644937701471316842
Mark Downie <poppastring@users.noreply.github.com>
2022-05-03 21:38:06 -0400

    Updated to only point to the jscript on the cdn. Removed the redundant local script.

--- source/DasBlog.Web.UI/TagHelpers/RichEdit/TinyMceBuilder.cs
+++ source/DasBlog.Web.UI/TagHelpers/RichEdit/TinyMceBuilder.cs
@@ -8,10 +8,9 @@ namespace DasBlog.Web.TagHelpers.RichEdit
 		private readonly IDasBlogSettings dasBlogSettings;
 		private const string TINY_MCE_SERVICE_URL = "https://cdn.tiny.cloud/1/{0}/tinymce/5/tinymce.min.js";
 		private const string INIT_SCRIPT_TEMPLATE = @"
-		<script language=""javascript"" type=""text/javascript"" src=""{0}/js/tinymce/plugins/code/plugin.min.js""></script>
 		<script>
 		tinymce.init({{
-			selector: '#{1}'
+			selector: '#{0}'
 			,plugins: 'code'
 		}});
 		</script>
@@ -40,7 +39,7 @@ namespace DasBlog.Web.TagHelpers.RichEdit
 			output.Attributes.SetAttribute("src", string.Format(TINY_MCE_SERVICE_URL, dasBlogSettings.SiteConfiguration.TinyMCEApiKey));
 			output.Attributes.SetAttribute("type", "text/javascript");
 			output.Attributes.SetAttribute("language", "javascript");
-			string htmlContent = string.Format(INIT_SCRIPT_TEMPLATE, dasBlogSettings.SiteConfiguration.Root, tagHeelper.ControlId);
+			string htmlContent = string.Format(INIT_SCRIPT_TEMPLATE, tagHeelper.ControlId);
 			output.PostElement.SetHtmlContent(htmlContent);
 		}
 	}
