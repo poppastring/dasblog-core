d765db4a9bf71aba89a2fb0aea8004af134e9105
Mark Downie <poppastring@users.noreply.github.com>
2025-05-20 20:21:42 -0400

    Add BlueSky sharing support to blog posts
    Added BlueSky as a new social media sharing option in `_BlogItem.cshtml`, alongside Twitter, Reddit, Facebook, and LinkedIn.
    
    Introduced `PostToBlueSkyTagHelper` to generate BlueSky sharing links, including post title and permalink encoding. Updated Razor views to ensure consistent integration of the BlueSky sharing feature across all relevant sections of the blog post footer.

---
+++ source/DasBlog.Web.UI/TagHelpers/Post/PostToBlueSkyTagHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using DasBlog.Services;
+using DasBlog.Web.Models.BlogViewModels;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace DasBlog.Web.TagHelpers
+{
+	public class PostToBlueSkyTagHelper : TagHelper
+	{
+		private const string BLUESKY_SHARE_URL = "https://bsky.app/intent/compose?text={0}";
+		private readonly IDasBlogSettings dasBlogSettings;
+		public PostViewModel Post { get; set; }
+
+		public PostToBlueSkyTagHelper(IDasBlogSettings dasBlogSettings)
+		{
+			this.dasBlogSettings = dasBlogSettings;
+		}
+
+		public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+		{
+			output.TagName = "a";
+			output.TagMode = TagMode.StartTagAndEndTag;
+			output.Attributes.SetAttribute("class", "dasblog-a-share-bluesky");
+			string text = $"{Post.Title} {dasBlogSettings.RelativeToRoot(Post.PermaLink)}";
+			output.Attributes.SetAttribute("href", string.Format(BLUESKY_SHARE_URL, UrlEncoder.Default.Encode(text)));
+
+			var content = await output.GetChildContentAsync();
+			output.Content.SetHtmlContent(content.GetContent());
+		}
+	}
+}
