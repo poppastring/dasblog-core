606929d0f87d341df9a5b6cfb9077a495f0c7d22
Mark Downie <poppastring@users.noreply.github.com>
2026-01-19 21:39:34 -0500

    Update nav links to use tag helpers; fix comment admin URLs
    Refactored navigation dropdown in _Layout.cshtml to use ASP.NET tag helpers for "Create Post", "Author", and "Activity" links, improving maintainability and route awareness. Updated comment management AJAX URLs in site.js to target the correct admin route. Removed the login button for unauthorized users from the layout.

--- source/DasBlog.Web.UI/wwwroot/js/site.js
+++ source/DasBlog.Web.UI/wwwroot/js/site.js
@@ -63,7 +63,7 @@ function executeCommentAction() {
     if (!action) return;
 
     const oReq = new XMLHttpRequest();
-    const url = 'post/' + action.postid + '/comments/' + action.commentid;
+    const url = '/admin/post/' + action.postid + '/comments/' + action.commentid;
 
     oReq.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
