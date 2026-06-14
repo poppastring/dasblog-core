907e8698392adf6fa153944cf2a720a81eb5bc38
Mark Downie <poppastring@users.noreply.github.com>
2026-06-13 23:28:17 -0400

    Use IUrlHelper to resolve site.css path in tag helper
    ThemeStylesheetsTagHelper now injects IUrlHelperFactory and ViewContext to generate the site.css URL using urlHelper.Content, ensuring correct path resolution across different hosting environments.

--- source/DasBlog.Web/TagHelpers/Site/ThemeStylesheetsTagHelper.cs
+++ source/DasBlog.Web/TagHelpers/Site/ThemeStylesheetsTagHelper.cs
@@ -1,4 +1,7 @@
 ﻿using DasBlog.Services;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 
 namespace DasBlog.Web.TagHelpers.Site
@@ -7,18 +10,26 @@ namespace DasBlog.Web.TagHelpers.Site
 	public class ThemeStylesheetsTagHelper : TagHelper
 	{
 		private readonly IDasBlogSettings dasBlogSettings;
+		private readonly IUrlHelperFactory urlHelperFactory;
 
-		public ThemeStylesheetsTagHelper(IDasBlogSettings dasBlogSettings)
+		public ThemeStylesheetsTagHelper(IDasBlogSettings dasBlogSettings, IUrlHelperFactory urlHelperFactory)
 		{
 			this.dasBlogSettings = dasBlogSettings;
+			this.urlHelperFactory = urlHelperFactory;
 		}
+
+		[ViewContext]
+		public ViewContext ViewContext { get; set; }
 
 		public override void Process(TagHelperContext context, TagHelperOutput output)
 		{
 			output.TagName = null;
 			output.TagMode = TagMode.StartTagAndEndTag;
 
-			var siteCss = "<link rel=\"stylesheet\" href=\"/css/site.css\" />";
+			var urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
+			var siteCssHref = urlHelper.Content("~/css/site.css");
+
+			var siteCss = $"<link rel=\"stylesheet\" href=\"{siteCssHref}\" />";
 			var themeCss = $"<link rel=\"stylesheet\" href=\"{dasBlogSettings.ThemeCssUrl}\" />";
 
 			output.Content.SetHtmlContent(siteCss + System.Environment.NewLine + themeCss);
